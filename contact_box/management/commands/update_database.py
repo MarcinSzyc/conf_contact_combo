770b8575f94b529f653323c6d29cfd647a17b116
MarcinSzyc <szyc.marcin@gmail.com>
2019-04-23 13:51:35 +0200

    dodana komenda update_database wykorzystujaca faker

---
+++ contact_box/management/commands/update_database.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from contact_box.models import Person, PhoneNumber, MOBILE_TYPES, Address, Group, Email, EMAIL_TYPE
+from random import choice, randint
+from contact_box.worker import *
+
+
+class Command(BaseCommand):
+    help = 'Fill database with fake data'
+
+    def handle(self, *args, **kwargs):
+        for i in range(100):
+            populate_initial_data()
+
+        for i in range(10):
+            create_groups()
+            for person in Person.objects.all():
+                add_address_to_person(person, choice(Address.objects.all()))
+                add_email_to_person(person, choice(Email.objects.all()))
+                add_group_to_person(person, choice(Group.objects.all()))
+                add_phone_to_person(person, choice(PhoneNumber.objects.all()))
---
+++ contact_box/worker.py
@@ -0,0 +1,41 @@
+from .models import Person, PhoneNumber, MOBILE_TYPES, Address, Group, Email, EMAIL_TYPE
+from faker import Faker
+from random import choice, randint
+
+
+def populate_initial_data():
+    fake = Faker()
+    person_instance = Person(name=fake.first_name(), last_name=fake.last_name(), description=fake.text())
+    phone_instance = PhoneNumber(phone_number=fake.msisdn(), type=choice(MOBILE_TYPES)[0])
+    address_instance = Address(city=fake.city(), street=fake.street_name(), home_number=randint(0, 1000),
+                               flat_number=randint(0, 100))
+    email_instance = Email(email_address=fake.email(), type=choice(EMAIL_TYPE)[0])
+    phone_instance.save()
+    person_instance.save()
+    address_instance.save()
+    email_instance.save()
+
+
+def create_groups():
+    fake = Faker()
+    group_instance = Group(name=fake.color_name())
+    group_instance.save()
+
+
+def add_address_to_person(person, address):
+    person.address = address
+    person.save()
+
+
+def add_email_to_person(person, email):
+    email.person = person
+    email.save()
+
+
+def add_phone_to_person(person, phone):
+    phone.person = person
+    phone.save()
+
+
+def add_group_to_person(person, group):
+    group.person.add(person)
