ad734a9872f9509bd166808ade2ec96abbf169f0
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-07 20:37:09 +0200

    general tests added

--- general/tests.py
+++ general/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class GeneralEndPointsTest(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get('', format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view(self):
+        response = self.client.get('/login/', format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_view(self):
+        response = self.client.get('/register/', format='json')
+        self.assertEqual(response.status_code, 200)
