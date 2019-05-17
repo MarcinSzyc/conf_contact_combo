69301c3c1a117710dc6c654cca1b80b9c35b6680
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-17 10:32:47 +0200

    added password reset form to context processor

--- general/login_context_processor.py
+++ general/login_context_processor.py
@@ -1,9 +1,11 @@
 from .forms import UserLogin, UserRegistration
+from django.contrib.auth.forms import PasswordResetForm
 
 
 def login_form_context(request):
     result = {
         'login_form': UserLogin,
-        'register_form': UserRegistration
+        'register_form': UserRegistration,
+        'password_reset_form': PasswordResetForm
     }
     return result
